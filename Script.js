2b9ad96ce9fbf5de1c5fabc68b0d833eb456924c
Toxic-Gruffalo <oliverendymioncohen@icloud.com>
2026-06-02 21:09:40 -0400

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,66 @@
+function checkIsPower(number, power) {
+  var isTrue;
+  var i = 1;
+  while (i <= number) {
+    if (i === number) {
+      isTrue = true;
+    } else {
+      isTrue = false;
+    }
+    i = i*power;
+  }
+  return isTrue;
+}
+
+function findFactor(number, power) {
+  for (var i = number; true; i--) {
+    if (checkIsPower(i, power)) {
+      return i;
+      break;
+    }
+  }
+}
+
+function getFactors(number, power) {
+  var factorList = [];
+  var i = number;
+  while (i >= 1) {
+    factorList.push(findFactor(i, power));
+    i = i - findFactor(i, power);
+  }
+  return factorList;
+}
+
+function createDigits(number, base) {
+  var digits = [];
+  var factors = getFactors(number, base);
+  var i = factors[0];
+  var temp = 0;
+  while (i >= 1) {
+    while (factors.includes(i)) {
+    temp++;
+    factors.splice(0, 1);
+    }
+    digits.push(temp);
+    i = i/base;
+    temp = 0;
+  }
+  return digits;
+}
+
+function createString(number, base) {
+  var string = '';
+  var digits = createDigits(number, base);
+  var i = 0
+  for (var i = 0; i < digits.length; i++) {
+    string = `${string}${digits[i]}`;
+  }
+  return string;
+}
+
+function displayNumber() { 
+  var number = Number(document.getElementById('input').value);
+  var base = Number(document.getElementById('base').value);
+document.getElementById('display').innerHTML = createString(number, base);
+  document.getElementById('display').style.padding = '3px'
+}
